d3ee158723863a03eefb31422e4d87467118ce5e
Sachin, Joshi(sacjoshi) <sacjoshi@ebay.com>
2018-05-16 08:22:51 -0700

    More problems

--- javascript/multiplication-word-problem-templates.js
+++ javascript/multiplication-word-problem-templates.js
@@ -4,5 +4,8 @@ var multiplicationWordProblemsList = [
   "-NAME1- is setting up -FIRST- remote control robots. Each robot needs -SECOND- batteries to operate. How many batteries does -NAME1- need for her remote control robots?",
   "-NAME1-'s horse ran -FIRST- meters. -NAME2-'s rocket-powered robo-ostrich ran -SECOND- times as far as -NAME1-'s horse. Find how far NAME2's robo-ostrich ran?",
   "-NAME1- is examining peas to try to understand how traits are passed from parents to offspring. Today, -NAME1- has -FIRST- peas to examine from each pod. He got these peas from -SECOND- pea pods. How many peas -NAME1- need to examine?",
-  "-NAME1- is making -FIRST- jars of banana flavored peanut butter. -NAME1- wants to put -FIRST- drops of banana flavoring in each jar. How many total drops of banana flavoring will -NAME1- need to make?"
+  "-NAME1- is making -FIRST- jars of banana flavored peanut butter. -NAME1- wants to put -FIRST- drops of banana flavoring in each jar. How many total drops of banana flavoring will -NAME1- need to make?",
+  "-NAME1- goes out to lunch with -SECOND- friends. Each person orders the $-FIRST- lunch special. -NAME1- agrees to pay the bill. How much will she have to pay ?",
+  "There were a total of -FIRST- baseball games a month. The season is played for -SECOND- months. How many baseball games are in the seasons ?",
+  "A restaurant sold -FIRST- hamburgers every day for -SECOND- Days. How many hamburgers were sold after -SECOND- Days?"
 ];
