7276635d72ed96cf7c83199019c90e0384eee358
Sachin, Joshi(sacjoshi) <sacjoshi@ebay.com>
2018-05-16 10:39:20 -0700

    Minor fix

--- javascript/multiplication-word-problem-templates.js
+++ javascript/multiplication-word-problem-templates.js
@@ -2,7 +2,7 @@ var multiplicationWordProblemsList = [
   "-NAME1- is setting up -FIRST- remote control drones. Each drone needs -SECOND- batteries to operate. How many batteries does -NAME1- need for her remote control drones?",
   "-NAME1- is setting up -FIRST- remote control cars. Each car needs -SECOND- batteries to operate. How many batteries does -NAME1- need for her remote control cars?",
   "-NAME1- is setting up -FIRST- remote control robots. Each robot needs -SECOND- batteries to operate. How many batteries does -NAME1- need for her remote control robots?",
-  "-NAME1-'s horse ran -FIRST- meters. -NAME2-'s rocket-powered robo-ostrich ran -SECOND- times as far as -NAME1-'s horse. Find how far NAME2's robo-ostrich ran?",
+  "-NAME1-'s horse ran -FIRST- meters. -NAME2-'s rocket-powered robo-ostrich ran -SECOND- times as far as -NAME1-'s horse. Find how far -NAME2-'s robo-ostrich ran?",
   "-NAME1- is examining peas to try to understand how traits are passed from parents to offspring. Today, -NAME1- has -FIRST- peas to examine from each pod. He got these peas from -SECOND- pea pods. How many peas -NAME1- need to examine?",
   "-NAME1- is making -FIRST- jars of banana flavored peanut butter. -NAME1- wants to put -FIRST- drops of banana flavoring in each jar. How many total drops of banana flavoring will -NAME1- need to make?",
   "There were a total of -FIRST- baseball games a month. The season is played for -SECOND- months. How many baseball games are in the seasons ?",
