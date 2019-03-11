2a6e45841eaecbfc9a8b589cab398edf276e6531
Sachin, Joshi(sacjoshi) <sacjoshi@ebay.com>
2019-03-11 08:16:28 -0700

    Reports are html now
    Reports are html now

---
+++ javascript/report.js
@@ -0,0 +1,41 @@
+var Report = Report || {};
+
+Report = {
+   readAuditTrail : function() {
+    var auditTrail =  StorageUtils.getItem("AUDIT");
+    if(typeof(auditTrail) == 'string') {
+      auditTrail = JSON.parse(auditTrail);
+    }
+    return auditTrail || [];
+  },
+   recordSuccess : function(problem, selectedAnswer) {
+    var auditTrail = readAuditTrail();
+    auditTrail.push({problem : problem, status: "correct", inputAnswer : selectedAnswer, timestamp : Date.now()});
+    StorageUtils.setItem("AUDIT",JSON.stringify(auditTrail));
+  },
+  recordFailure : function(problem, selectedAnswer){
+    var auditTrail = readAuditTrail();
+    auditTrail.push({problem : problem, status: "wrong", inputAnswer : selectedAnswer, timestamp : Date.now()});
+    StorageUtils.setItem("AUDIT",JSON.stringify(auditTrail));
+  },
+  toTable : function() {
+    var html = '';
+    var auditTrail = Report.readAuditTrail();
+    if(auditTrail && typeof(auditTrail) == 'array') {
+      html+='<table>';
+      html+='<tr><th>Problem</th><th>Attempt Result</th><th>Attempt Answer</th><th>Date</th></tr>'
+      auditTrail.forEach(function(row){
+          html+='<tr>';
+          html+='<td>' + row.problem + '</td>';
+          html+='<td>' + row.status + '</td>';
+          html+='<td>' + row.inputAnswer + '</td>';
+          var date = new Date(row.timestamp);
+          html+='<td>' + date.getMonth() + '/' + date.getDate() + '/' + date.getYear() + '</td>';
+          html+='</tr>';
+      });
+      html+='</table>';
+    }
+
+    return html;
+  }
+};
