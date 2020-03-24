2fce74b031a36cb7dd4e668abef293806778cf91
Sachin, Joshi(sacjoshi) <sacjoshi@ebay.com>
2020-03-24 11:36:29 -0700

    All Problems Page
    All Problems Page

---
+++ javascript/problemdb.js
@@ -0,0 +1,53 @@
+var ProblemDatabase = ProblemDatabase || {};
+
+ProblemDatabase = {
+  findAll : function() {
+    var problemDb =  StorageUtils.getItem("PROBLEMDB");
+    if(typeof(problemDb) == 'string') {
+      problemDb = JSON.parse(problemDb);
+    }
+    return problemDb || [];
+  },
+   add : function(problem) {
+    StorageUtils.setItem("PROBLEMDB" + '.' + problem.id(),JSON.stringify(problem));
+    var allProblems = new Map(Report.findAll());
+    allProblems.set(problem.id(), problem);
+    StorageUtils.setItem("PROBLEMDB",JSON.stringify(Array.from(allProblems.entries())));
+  },
+   findById : function(id) {
+     var problem =  StorageUtils.getItem("PROBLEMDB" + '.' + id);
+     if(typeof(problem) == 'string') {
+       problem = JSON.parse(problem);
+     }
+     return problem || {};
+  },
+  toTable : function(filter) {
+    var html = '';
+    var problemDb = ProblemDatabase.findAll();
+    if(problemDb && problemDb.length > 0) {
+      const showProblem = function(row) {
+        try {
+          const dp = (Problems && Problems.createByType && row.problem && row.problem.subtype && Problems.createByType[row.problem.subtype])
+            ? Problems.createByType[row.problem.subtype](row.problem)
+            : '';
+          return (dp && dp.displayProblem) ? dp.displayProblem() : 'Cant Serialize: ' + (row.problem.subtype || 'Unknown');
+        } catch(e) {
+          return 'Failed to show problem: missing dep?';
+        }
+      };
+      html+='<table>';
+      html+='<tr><th>ProblemID</th><th>Problem</th><th>Type</th></tr>';
+      problemDb.forEach(function(row){
+          if(filter && row.status !== filter) return;
+          html+='<tr>';
+          html+='<td>' + row[0] + '</td>';
+          html+='<td>' + showProblem(row[1]) + '</td>';
+          html+='<td>' + row[1].subtype + '</td>';
+          html+='</tr>';
+      });
+      html+='</table>';
+    }
+
+    return html;
+  }
+};
