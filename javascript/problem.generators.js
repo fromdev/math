82566e3448369594e4294e05af9a99b0c32c9adf
Sachin, Joshi(sacjoshi) <sacjoshi@ebay.com>
2020-03-25 10:42:45 -0700

    Added negative numbers in algebra equations
    Added negative numbers in algebra equations

--- javascript/problem.generators.js
+++ javascript/problem.generators.js
@@ -37,6 +37,8 @@ Problems.Generators = {
         var b = RandomUtils.getRandomInt(level.range.end/2 + level.range.end);
         problems.push(new SimpleAddEquation(a,b));
         problems.push(new SimpleSubtractEquation(a,b));
+        problems.push(new SimpleAddEquation(a,-b));
+        problems.push(new SimpleSubtractEquation(a,-b));
         a = RandomUtils.getRandomInt(1,20);
         b = a * RandomUtils.getRandomInt(1,5);
         problems.push(new SimpleMultiplyEquation(a,b));
