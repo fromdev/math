7feb148a0e4c198b8a2b47b3dfae032ff7705f41
Sachin Joshi <sacjoshi@ebay.com>
2021-04-16 15:37:33 -0700

    format

--- javascript/report.js
+++ javascript/report.js
@@ -60,7 +60,7 @@ Report = {
       const ids = auditTrail.map(row => row.id);
       const uniques = new Set(ids);
       html += `<div>Unique Questions: ${uniques.size}</div>`;
-      html += `<div>Redeemable Coins: ${uniques.size*10}</div>`;
+      html += `<div>Redeemable Coins: <strong style="color:green">${uniques.size*10}</strong></div>`;
     } 
 
     return html;
